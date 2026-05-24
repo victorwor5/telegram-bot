38a9b0db1a17b5bb845446a4e90a395c32efc2cd
victorwor5 <joinedenschoolofsounds@gmail.com>
2026-05-24 11:16:28 +0100

    Add subscription plan buttons to bot start message

--- bot.js
+++ bot.js
@@ -1,10 +1,27 @@
 require("dotenv").config();
-const { Telegraf } = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// Start command
 bot.start((ctx) => {
-    ctx.reply("Hello 👋 Bot is working!");
+    ctx.reply(
+        "Welcome 👋\nChoose a subscription plan:",
+        Markup.inlineKeyboard([
+            [Markup.button.callback("🎟 Tier 1", "tier1")],
+            [Markup.button.callback("🔥 Tier 2", "tier2")]
+        ])
+    );
+});
+
+// Tier 1 button
+bot.action("tier1", (ctx) => {
+    ctx.reply("You selected Tier 1");
+});
+
+// Tier 2 button
+bot.action("tier2", (ctx) => {
+    ctx.reply("You selected Tier 2");
 });
 
 bot.launch();
